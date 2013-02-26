036842a6be24534e106ca6d46d9b3fcb98648ee6
Tarlusk <wetsocsid@lavabit.com>
2013-02-25 21:10:30 -0800

    Made Main and Game able to be launched.

--- src/Game.java
+++ src/Game.java
@@ -6,27 +6,27 @@ import org.newdawn.slick.SlickException;
 
 public class Game extends BasicGame{
 
-	public Game(String title) {
+	public Game(String title) 
+	{
 		super(title);
-		// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
+	public void init(GameContainer arg0) throws SlickException 
+	{
 		
 	}
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
+	public void update(GameContainer gc, int g) throws SlickException 
+	{
 		
 	}
-
+	
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
+	public void render(GameContainer gc, Graphics g) throws SlickException 
+	{
+		g.drawString("Hello World", 100, 100);
 		
 	}
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 
 public class Main 
 {
@@ -5,5 +8,14 @@ public class Main
 	public static void main (String args[])
 	{
 		System.out.println("Hello World");
+
+		try {
+			AppGameContainer app = new AppGameContainer(new Game("Hello World"));
+			app.setDisplayMode(800, 600, false);
+			app.start();
+		} 
+		catch (SlickException e) {
+			e.printStackTrace();
+		}
 	}
 }
