3b1c8b634bd2b05678e4cf3c8ddc8aed2f900d51
Tarlusk <wetsocsid@lavabit.com>
2013-02-25 22:21:47 -0800

    Cleaned up Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
-
 public class Main 
 {
-
 	public static void main (String args[])
 	{
 		System.out.println("Hello World");
 
-		try {
+		try 
+		{
 			AppGameContainer app = new AppGameContainer(new Game("Hello World"));
 			app.setDisplayMode(800, 600, false);
 			app.start();
 		} 
-		catch (SlickException e) {
+		catch (SlickException e) 
+		{
 			e.printStackTrace();
 		}
 	}
