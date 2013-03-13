4eec39caadba6f759f85f075116ca43e5b5f01fd
Tarlusk <wetsocsid@lavabit.com>
2013-03-12 19:31:36 -0700

    Changed attack command into 4 new commands attackUp, attackDown,attackLeft,attackRight

--- src/controls/ControlProvider.java
+++ src/controls/ControlProvider.java
@@ -11,7 +11,11 @@ import org.newdawn.slick.command.MouseButtonControl;
 
 public class ControlProvider extends InputProvider{
 
-	public Command attack = new BasicCommand("attack");
+	public Command attackLeft = new BasicCommand("attackLeft");
+	public Command attackRight = new BasicCommand("attackRight");
+	public Command attackUp = new BasicCommand("attackUP");
+	public Command attackDown = new BasicCommand("attackDown");
+	
 	public Command jump = new BasicCommand("jump");
 	public Command left = new BasicCommand("left");
 	public Command right = new BasicCommand("right");
@@ -41,15 +45,16 @@ public class ControlProvider extends InputProvider{
 	public ControlProvider(Input input) {
 		super(input);
 		
-        this.bindCommand(new MouseButtonControl(0), attack);
+        this.bindCommand(new MouseButtonControl(0), attackUp);
 		
-        this.bindCommand(new KeyControl(Input.KEY_LEFT), left);
+        this.bindCommand(new KeyControl(Input.KEY_LEFT), attackLeft);
+        this.bindCommand(new KeyControl(Input.KEY_RIGHT), attackRight);
+        this.bindCommand(new KeyControl(Input.KEY_DOWN), attackDown);
+        this.bindCommand(new KeyControl(Input.KEY_UP), attackUp);
+        
         this.bindCommand(new KeyControl(Input.KEY_A), left);
-        this.bindCommand(new KeyControl(Input.KEY_RIGHT), right);
         this.bindCommand(new KeyControl(Input.KEY_D), right);
-        this.bindCommand(new KeyControl(Input.KEY_UP), up);
         this.bindCommand(new KeyControl(Input.KEY_W), up);
-        this.bindCommand(new KeyControl(Input.KEY_DOWN), down);
         this.bindCommand(new KeyControl(Input.KEY_S), down);
         this.bindCommand(new KeyControl(Input.KEY_SPACE), jump);
         this.bindCommand(new KeyControl(Input.KEY_ESCAPE), pause);
@@ -58,7 +63,6 @@ public class ControlProvider extends InputProvider{
         this.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.RIGHT), right);
         this.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.UP), up);
         this.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.DOWN), down);
-        this.bindCommand(new ControllerButtonControl(0, 14), attack);
         
         this.bindCommand(new ControllerButtonControl(0, 0), ps);
     	this.bindCommand(new ControllerButtonControl(0, 1), select);
